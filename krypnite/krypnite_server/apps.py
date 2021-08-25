a643c4eeae9e783aef12052d06e99b971ec02889
Udrescu Vlad <udrescumvlad@gmail.com>
2021-08-25 11:57:16 +0300

    remake the project from 0

--- krypnite/krypnite_server/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KrypniteServerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'krypnite_server'
--- krypnite/krypnite_trackingsystem/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KrypniteTrackingsystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'krypnite_trackingsystem'
