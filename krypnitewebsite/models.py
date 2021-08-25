003ab93278d9b2b6a7126b400babe8d409861d72
Udrescu Vlad <udrescumvlad@gmail.com>
2021-08-25 22:41:27 +0300

    db now working fine

--- krypnitewebsite/models.py
+++ krypnitewebsite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    image = models.ImageField(null=True,blank=True)
+    description = models.CharField(max_length=400,null=True)
+    origin = models.CharField(max_length=10, null=True)
+    def __str__(self):
+        return self.name
